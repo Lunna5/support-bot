010e686bcf61df4b9a6c4e9c46c30aa02fd096c6
Lunna5 <git@lunna.dev>
2025-04-24 19:02:49 +0200

    chore: refactor BotConfig to use Map for questionAnswers

--- src/main/java/dev/lunna/support/bot/api/config/BotConfig.java
+++ src/main/java/dev/lunna/support/bot/api/config/BotConfig.java
@@ -4,13 +4,13 @@ import dev.lunna.support.bot.api.data.QuestionAnswer;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 import org.spongepowered.configurate.objectmapping.meta.Setting;
 
-import java.util.List;
+import java.util.Map;
 
 @ConfigSerializable
 public class BotConfig {
     @Setting
-    public List<QuestionAnswer> questionAnswers = List.of(
-            new QuestionAnswer("Question 1", new String[]{"Catchphrase 1"}, "Answer 1"),
-            new QuestionAnswer("Question 2", new String[]{"Catchphrase 2"}, "Answer 2")
+    public Map<String, QuestionAnswer> questionAnswers = Map.of(
+            "question1", new QuestionAnswer("Question 1", new String[]{"Catchphrase 1"}, "Answer 1"),
+            "question2", new QuestionAnswer("Question 2", new String[]{"Catchphrase 2"}, "Answer 2")
     );
 }
